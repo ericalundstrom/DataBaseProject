2447fee04c5f9358f965d463906a4374ba2bc5cd
Grace <yintingchancytg@gmail.com>
2024-12-07 01:06:52 +0100

    Removed redundant code

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: 'pgserver.mah.se',
   port: 5432,
-  user: 'an3937',
-  password: '82q1bygc',
-  database: 'conference_management',
+  user: '',
+  password: '',
+  database: '',
 });
 
 const connectDatabase = async () => {
