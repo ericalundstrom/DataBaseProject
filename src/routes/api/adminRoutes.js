aed9ab7ae3ee52c2327bfae2fbd203957271d5c8
ericalundstrom <erica.lundstrom99@hotmail.com>
2025-01-07 20:31:58 +0100

    updated

--- src/routes/api/adminRoutes.js
+++ src/routes/api/adminRoutes.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post('/create-submission', AdminController.createSubmission);
 router.get('/assign-reviewer', AdminController.getArticlesToAssignReviewer);
-router.get('/assign-reviewer', AdminController.getReviewers);
 router.post('/assign-reviewer', AdminController.assignReviewers);
 router.get('/all-articles', AdminController.getAllArticles);
 router.get('/remove-reviewer', AdminController.getAllReviewers);
